1dd46eec6288e30729640ebf3967c5d539eedc58
Chao He <huantaihechao@163.com>
2022-10-28 12:56:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ from torchmetrics import MeanMetric, Accuracy
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train')
-    parser.add_argument('--cwru_data', type=str, default="H:\\DDTLN\\12k_de_cwru_data_load2_0.007.npy", help='')
-    parser.add_argument('--cwru_label', type=str, default="H:\\DDTLN\\12k_de_cwru_label_load2_0.007.npy", help='')
+    parser.add_argument('--cwru_data', type=str, default="H:\\DDTLN\\48kcwru_data.npy", help='')
+    parser.add_argument('--cwru_label', type=str, default="H:\\DDTLN\\48kcwru_label.npy", help='')
     parser.add_argument('--bjtu_data', type=str, default="H:\\DDTLN\\gt300data_data.npy", help='')
     parser.add_argument('--bjtu_label', type=str, default="H:\\DDTLN\\gt300data_label.npy", help='')
-    parser.add_argument('--batch_size', type=int, default=128, help='batchsize of the training process')
-    parser.add_argument('--nepoch', type=int, default=1000, help='max number of epoch')
-    parser.add_argument('--num_classes', type=int, default=4, help='max number of epoch')
+    parser.add_argument('--batch_size', type=int, default=256, help='batchsize of the training process')
+    parser.add_argument('--nepoch', type=int, default=100, help='max number of epoch')
+    parser.add_argument('--num_classes', type=int, default=4, help='')
     parser.add_argument('--losses', type=list, default=[], help='')
-    parser.add_argument('--lr', type=float, default=0.001, help='initialization list')
+    parser.add_argument('--lr', type=float, default=0.001, help='')
     parser.add_argument('--weight_decay', type=float, default=0.0001, help='initialization list')
     args = parser.parse_args()
     return args
@@ -48,8 +48,8 @@ def load_data():
     target_data = MinMaxScaler().fit_transform(target_data.T).T
     source_data = np.expand_dims(source_data, axis=1).astype('float32')
     target_data = np.expand_dims(target_data, axis=1).astype('float32')
-    source_data, source_label = shuffle(source_data, source_label, random_state=2)
-    target_data, target_label = shuffle(target_data, target_label, random_state=2)
+    # source_data, source_label = shuffle(source_data, source_label, random_state=2)
+    # target_data, target_label = shuffle(target_data, target_label, random_state=2)
     Train_source = Dataset(source_data, source_label)
     Train_target = Dataset(target_data, target_label)
     return Train_source, Train_target
@@ -77,7 +77,7 @@ def _mix_rbf_kernel(X, Y, sigmas, wts=None):
 
     K_XX, K_XY, K_YY = 0., 0., 0.
     for sigma, wt in zip(sigmas, wts):
-        gamma = 1 / (2 * sigma ** 2)
+        gamma = 1 / (2 * sigma ** 2 + 1e-5)
         K_XX += wt * torch.exp(-gamma * (-2 * XX + c(X_sqnorms) + r(X_sqnorms)))
         K_XY += wt * torch.exp(-gamma * (-2 * XY + c(X_sqnorms) + r(Y_sqnorms)))
         K_YY += wt * torch.exp(-gamma * (-2 * YY + c(Y_sqnorms) + r(Y_sqnorms)))
@@ -86,9 +86,9 @@ def _mix_rbf_kernel(X, Y, sigmas, wts=None):
 
 def _mmd2(m, n, K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
     if biased:
-        mmd2 = ((torch.sum(K_XX) / (m * m))
-                + (torch.sum(K_YY) / (n * n))
-                - (2 * torch.sum(K_XY) / (m * n))).float()
+        mmd2 = ((torch.sum(K_XX) / (m * m + 1e-5))
+                + (torch.sum(K_YY) / (n * n + 1e-5))
+                - (2 * torch.sum(K_XY) / (m * n + 1e-5))).float()
     else:
         if const_diagonal is not False:
             trace_X = m * const_diagonal
@@ -105,25 +105,25 @@ def _mmd2(m, n, K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
 
 def MDA(m, n, source, target, bandwidths=[1]):
 
-    kernel_loss = _mix_rbf_mmd2(m, n, source, target, sigmas=bandwidths) * 10
+    kernel_loss = _mix_rbf_mmd2(m, n, source, target, sigmas=bandwidths) * 100.
     eps = 1e-5
     d = source.size()[1]
     ns, nt = source.size()[0], target.size()[0]
 
     # source covariance
     tmp_s = torch.matmul(torch.ones(size=(1, ns)).cuda(), source)
-    cs = (torch.matmul(torch.t(source), source) - torch.matmul(torch.t(tmp_s), tmp_s) / ns) / (ns - 1 + eps) * (
-                ns / m)
+    cs = (torch.matmul(torch.t(source), source) - torch.matmul(torch.t(tmp_s), tmp_s) / (ns + eps)) / (ns - 1 + eps) * (
+                ns / (m + eps))
 
     # target covariance
     tmp_t = torch.matmul(torch.ones(size=(1, nt)).cuda(), target)
-    ct = (torch.matmul(torch.t(target), target) - torch.matmul(torch.t(tmp_t), tmp_t) / nt) / (nt - 1 + eps) * (
-                nt / n)
+    ct = (torch.matmul(torch.t(target), target) - torch.matmul(torch.t(tmp_t), tmp_t) / (nt + eps)) / (nt - 1 + eps) * (
+                nt / (n + eps))
     # frobenius norm
     loss = torch.sqrt(torch.sum(torch.pow((cs - ct), 2)))
-    loss = loss / (4 * d * d)
+    loss = loss / (4 * d * d) * 10.
 
-    return float(loss) + kernel_loss
+    return loss + kernel_loss
 
 def _if_list(list):
     if len(list) == 0:
@@ -154,18 +154,13 @@ def CDA(m, n, output1, source_label, output2, pseudo_label):
     t_0, t_1, t_2, t_3 = _classification_division(output2, pseudo_label)
 
     CDA_loss = 0
-    count = 0
     if t_0.size()[0] != 0:
-        count += 1
         CDA_loss += MDA(m, n, s_0, t_0)
     if t_1.size()[0] != 0:
-        count += 1
         CDA_loss += MDA(m, n, s_1, t_1)
     if t_2.size()[0] != 0:
-        count += 1
         CDA_loss += MDA(m, n, s_2, t_2)
     if t_3.size()[0] != 0:
-        count += 1
         CDA_loss += MDA(m, n, s_3, t_3)
     return CDA_loss / 4.
 
@@ -178,8 +173,6 @@ class I_Softmax(nn.Module):
         self.n = torch.tensor([n]).cuda()
         self.source_output1 = source_output1
         self.source_label = source_label
-        self.criterion = nn.CrossEntropyLoss()
-        # self.label_argmax = torch.argmax(self.source_label, dim=-1)
         self.la, self.lb, self.lc, self.ld = [], [], [], []
         self.a, self.b, self.c, self.d = [], [], [], []
         self.data_set = []
@@ -278,26 +271,25 @@ class DDTLN(nn.Module):
                                   nn.ReLU())
         self.p5_2 = nn.MaxPool1d(2, 2)
         self.p6 = nn.AdaptiveAvgPool1d(1)
-        self.p7_1 = nn.Sequential(nn.Linear(512, 512),
-                                  nn.ReLU())
-        self.p7_2 = nn.Sequential(nn.utils.weight_norm(nn.Linear(512, 4), name='weight'))
-        self._weights_init()
-    def forward(self, x):
+        self.p7_1 = nn.Sequential(nn.Linear(512, 128))
+        self.p7_2 = nn.Sequential(nn.utils.weight_norm(nn.Linear(128, 4), name='weight'))
+        # self._weights_init()
+    def forward(self, x, y):
         x = self.p1_2(self.p1_1(x))
         x = self.p2_2(self.p2_1(x))
         x = self.p3_2(self.p3_1(x))
         x = self.p4_2(self.p4_1(x))
         x = self.p5_2(self.p5_1(x))
-        x = self.p7_1(self.p6(x).squeeze())
-        x = self.p7_2(x)
-        # y = self.p1_2(self.p1_1(y))
-        # y = self.p2_2(self.p2_1(y))
-        # y = self.p3_2(self.p3_1(y))
-        # y = self.p4_2(self.p4_1(y))
-        # y = self.p5_2(self.p5_1(y))
-        # y = self.p7_1(self.p6(y).squeeze())
-        # y = self.p7_2(y)
-        return x
+        x1 = self.p7_1(self.p6(x).squeeze())
+        x2 = self.p7_2(x1)
+        y = self.p1_2(self.p1_1(y))
+        y = self.p2_2(self.p2_1(y))
+        y = self.p3_2(self.p3_1(y))
+        y = self.p4_2(self.p4_1(y))
+        y = self.p5_2(self.p5_1(y))
+        y1 = self.p7_1(self.p6(y).squeeze())
+        y2 = self.p7_2(y1)
+        return x1, x2, y1, y2
 
     def _weights_init(self):
         for L in self.modules():
@@ -318,7 +310,7 @@ def train(model, epoch, source_loader, target_loader, optimizer):
     iter_source = iter(source_loader)
     iter_target = iter(target_loader)
     num_iter = len(source_loader)
-    lambd = 2 / (1 + np.exp(-10 * (epoch) / args.nepoch)) - 1
+    lambd = -4 / (np.sqrt(epoch / (args.nepoch - epoch + 1)) + 1) + 4
     for i in range(0, num_iter):
         source_data, source_label = next(iter_source)
         target_data, target_label = next(iter_target)
@@ -327,20 +319,18 @@ def train(model, epoch, source_loader, target_loader, optimizer):
         m = source_data.size()[0]
         n = target_data.size()[0]
         optimizer.zero_grad()
-        output1 = model(source_data)                    #, output2, target_data
-        output2 = model(target_data)
-        data, label, clc_loss_step = I_Softmax(1, 0, output1, source_label).forward()
-        pre_pseudo_label = torch.argmax(F.softmax(output2, dim=1), dim=-1)
-        pseudo_data, pseudo_label, pseudo_loss_step = I_Softmax(1, 0, output2, pre_pseudo_label).forward()
-        CDA_loss = CDA(m, n, output1, source_label, output2, pre_pseudo_label)
-        MDA_loss = MDA(m, n, output1, output2)
-        # loss_step = criterion(output1, source_label)
-        loss_step = clc_loss_step + (MDA_loss + CDA_loss * 0.1) + pseudo_loss_step * 0.1
+        out1, output1, out2, output2 = model(source_data, target_data)                    #, output2, target_data
+        data, label, clc_loss_step = I_Softmax(3, 16, output1, source_label).forward()
+        pre_pseudo_label = torch.argmax(F.softmax(output2, dim=-1), dim=-1)
+        pseudo_data, pseudo_label, pseudo_loss_step = I_Softmax(3, 16, output2, pre_pseudo_label).forward()
+        # = criterion(output1, source_label)
+        CDA_loss = CDA(m, n, out1, source_label, out2, pre_pseudo_label)
+        MDA_loss = MDA(m, n, out1, out2)
+        loss_step = clc_loss_step + (MDA_loss + 0.5 * CDA_loss) + 10 * pseudo_loss_step
         loss_step.backward()
-        # nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.01208, norm_type=2)
         optimizer.step()
-        metric_accuracy_1.update(F.softmax(data, dim=-1).max(1)[1], label)
-        metric_accuracy_2.update(F.softmax(output2, dim=-1).max(1)[1], target_label)
+        metric_accuracy_1.update(output1.max(1)[1], source_label)
+        metric_accuracy_2.update(output2.max(1)[1], target_label)
         metric_mean_1.update(loss_step)
         metric_mean_2.update(criterion(output1, source_label))
         metric_mean_3.update(criterion(output2, target_label))
@@ -349,7 +339,9 @@ def train(model, epoch, source_loader, target_loader, optimizer):
         metric_mean_6.update(CDA_loss)
         metric_mean_7.update(MDA_loss)
     train_acc = metric_accuracy_1.compute()
+    ###############################################
     test_acc = metric_accuracy_2.compute()
+    #################################################
     train_all_loss = metric_mean_1.compute()    # loss_step
     train_loss = metric_mean_2.compute()
     test_loss = metric_mean_3.compute()
@@ -393,7 +385,7 @@ if __name__ == '__main__':
     target_loader = da.DataLoader(dataset=Train_target, batch_size=args.batch_size, shuffle=True, generator=g)
     model = DDTLN().to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     model.train()
     for epoch in range(0, args.nepoch):
         stop += 1
