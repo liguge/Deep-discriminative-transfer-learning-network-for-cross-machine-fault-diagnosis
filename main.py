e1a2f25141004222b977311403e4d9446b26e424
Chao He <huantaihechao@163.com>
2022-10-28 14:07:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from pytorch_lightning.utilities.seed import seed_everything
 import torch
 import torch.nn as nn
@@ -10,21 +10,21 @@ from torch.utils import data as da
 from torchmetrics import MeanMetric, Accuracy
 
 
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Train')
-    parser.add_argument('--cwru_data', type=str, default="H:\\DDTLN\\48kcwru_data.npy", help='')
-    parser.add_argument('--cwru_label', type=str, default="H:\\DDTLN\\48kcwru_label.npy", help='')
-    parser.add_argument('--bjtu_data', type=str, default="H:\\DDTLN\\gt300data_data.npy", help='')
-    parser.add_argument('--bjtu_label', type=str, default="H:\\DDTLN\\gt300data_label.npy", help='')
+    parser.add_argument('--cwru_data', type=str, default="H:\\DDTLN\\gt300data_data.npy", help='')
+    parser.add_argument('--cwru_label', type=str, default="H:\\DDTLN\\gt300data_label.npy", help='')
+    parser.add_argument('--bjtu_data', type=str, default="H:\\DDTLN\\gt160data_data.npy", help='')
+    parser.add_argument('--bjtu_label', type=str, default="H:\\DDTLN\\gt160data_label.npy", help='')
     parser.add_argument('--batch_size', type=int, default=256, help='batchsize of the training process')
     parser.add_argument('--nepoch', type=int, default=100, help='max number of epoch')
     parser.add_argument('--num_classes', type=int, default=4, help='')
-    parser.add_argument('--losses', type=list, default=[], help='')
     parser.add_argument('--lr', type=float, default=0.001, help='')
     parser.add_argument('--weight_decay', type=float, default=0.0001, help='initialization list')
     args = parser.parse_args()
     return args
+
+
 class Dataset(da.Dataset):
     def __init__(self, X, y):
         self.Data = X
@@ -38,131 +38,131 @@ class Dataset(da.Dataset):
     def __len__(self):
         return len(self.Data)
 
-def load_data():
 
+def load_data():
     source_data = np.load(args.cwru_data)
     source_label = np.load(args.cwru_label).argmax(axis=-1)
     target_data = np.load(args.bjtu_data)
     target_label = np.load(args.bjtu_label).argmax(axis=-1)
-    source_data = MinMaxScaler().fit_transform(source_data.T).T
-    target_data = MinMaxScaler().fit_transform(target_data.T).T
-    source_data = np.expand_dims(source_data, axis=1).astype('float32')
-    target_data = np.expand_dims(target_data, axis=1).astype('float32')
-    # source_data, source_label = shuffle(source_data, source_label, random_state=2)
-    # target_data, target_label = shuffle(target_data, target_label, random_state=2)
+    source_data = StandardScaler().fit_transform(source_data.T).T
+    target_data = StandardScaler().fit_transform(target_data.T).T
+    source_data = np.expand_dims(source_data, axis=1)
+    target_data = np.expand_dims(target_data, axis=1)
+    source_data, source_label = shuffle(source_data, source_label, random_state=2)
+    target_data, target_label = shuffle(target_data, target_label, random_state=2)
     Train_source = Dataset(source_data, source_label)
     Train_target = Dataset(target_data, target_label)
     return Train_source, Train_target
 
 
 ###############################################################
+class MMD(nn.Module):
+    def __init__(self, m, n):
+        super(MMD, self).__init__()
+        self.m = m
+        self.n = n
 
+    def _mix_rbf_mmd2(self, X, Y, sigmas=(10,), wts=None, biased=True):
+        K_XX, K_XY, K_YY, d = self._mix_rbf_kernel(X, Y, sigmas, wts)
+        return self._mmd2(K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
 
-def _mix_rbf_mmd2(m, n, X, Y, sigmas=(10,), wts=None, biased=True):
-    K_XX, K_XY, K_YY, d = _mix_rbf_kernel(X, Y, sigmas, wts)
-    return _mmd2(m, n, K_XX, K_XY, K_YY, const_diagonal=d, biased=biased)
+    def _mix_rbf_kernel(self, X, Y, sigmas, wts=None):
+        if wts is None:
+            wts = [1] * len(sigmas)
+        XX = torch.matmul(X, X.t())
+        XY = torch.matmul(X, Y.t())
+        YY = torch.matmul(Y, Y.t())
 
-def _mix_rbf_kernel(X, Y, sigmas, wts=None):
-    if wts is None:
-        wts = [1] * len(sigmas)
-    XX = torch.matmul(X, X.t())
-    XY = torch.matmul(X, Y.t())
-    YY = torch.matmul(Y, Y.t())
+        X_sqnorms = torch.diagonal(XX)
+        Y_sqnorms = torch.diagonal(YY)
 
-    X_sqnorms = torch.diagonal(XX)
-    Y_sqnorms = torch.diagonal(YY)
+        r = lambda x: torch.unsqueeze(x, 0)
+        c = lambda x: torch.unsqueeze(x, 1)
 
-    r = lambda x: torch.unsqueeze(x, 0)
-    c = lambda x: torch.unsqueeze(x, 1)
+        K_XX, K_XY, K_YY = 0., 0., 0.
+        for sigma, wt in zip(sigmas, wts):
+            gamma = 1 / (2 * sigma ** 2)
+            K_XX += wt * torch.exp(-gamma * (-2 * XX + c(X_sqnorms) + r(X_sqnorms)))
+            K_XY += wt * torch.exp(-gamma * (-2 * XY + c(X_sqnorms) + r(Y_sqnorms)))
+            K_YY += wt * torch.exp(-gamma * (-2 * YY + c(Y_sqnorms) + r(Y_sqnorms)))
+            return K_XX, K_XY, K_YY, torch.sum(torch.tensor(wts))
 
-    K_XX, K_XY, K_YY = 0., 0., 0.
-    for sigma, wt in zip(sigmas, wts):
-        gamma = 1 / (2 * sigma ** 2 + 1e-5)
-        K_XX += wt * torch.exp(-gamma * (-2 * XX + c(X_sqnorms) + r(X_sqnorms)))
-        K_XY += wt * torch.exp(-gamma * (-2 * XY + c(X_sqnorms) + r(Y_sqnorms)))
-        K_YY += wt * torch.exp(-gamma * (-2 * YY + c(Y_sqnorms) + r(Y_sqnorms)))
-        return K_XX, K_XY, K_YY, torch.sum(torch.tensor(wts))
-
-
-def _mmd2(m, n, K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
-    if biased:
-        mmd2 = ((torch.sum(K_XX) / (m * m + 1e-5))
-                + (torch.sum(K_YY) / (n * n + 1e-5))
-                - (2 * torch.sum(K_XY) / (m * n + 1e-5))).float()
-    else:
-        if const_diagonal is not False:
-            trace_X = m * const_diagonal
-            trace_Y = n * const_diagonal
+    def _mmd2(self,K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
+        if biased:
+            mmd2 = torch.sum(K_XX) / (self.m * self.m) + torch.sum(K_YY) / (self.n * self.n)
+            - 2 * torch.sum(K_XY) / (self.m * self.n)
         else:
-            trace_X = torch.trace(K_XX)
-            trace_Y = torch.trace(K_YY)
+            if const_diagonal is not False:
+                trace_X = self.m * const_diagonal
+                trace_Y = self.n * const_diagonal
+            else:
+                trace_X = torch.trace(K_XX)
+                trace_Y = torch.trace(K_YY)
 
-        mmd2 = ((torch.sum(K_XX) - trace_X) / (m * (m - 1))
-                + (torch.sum(K_YY) - trace_Y) / (n * (n - 1))
-                - 2 * torch.sum(K_XY) / (m * n))
-    return mmd2
+            mmd2 = ((torch.sum(K_XX) - trace_X) / (self.m * (self.m - 1))
+                    + (torch.sum(K_YY) - trace_Y) / (self.n * (self.n - 1))
+                    - 2 * torch.sum(K_XY) / (self.m * self.n))
+        return mmd2
 
+    def _if_list(self, list):
+        if len(list) == 0:
+            list = torch.tensor(list)
+        else:
+            list = torch.vstack(list)
+        return list
 
-def MDA(m, n, source, target, bandwidths=[1]):
+    def _classification_division(self, data, label):
+        N = data.size()[0]
+        a, b, c, d = [], [], [], []
+        for i in range(N):
+            if label[i] == 0:
+                a.append(data[i])  # .detach().cpu().numpy()
+            elif label[i] == 1:
+                b.append(data[i])
+            elif label[i] == 2:
+                c.append(data[i])
+            elif label[i] == 3:
+                d.append(data[i])
 
-    kernel_loss = _mix_rbf_mmd2(m, n, source, target, sigmas=bandwidths) * 100.
-    eps = 1e-5
-    d = source.size()[1]
-    ns, nt = source.size()[0], target.size()[0]
+        return self._if_list(a), self._if_list(b), self._if_list(c), self._if_list(d)
 
-    # source covariance
-    tmp_s = torch.matmul(torch.ones(size=(1, ns)).cuda(), source)
-    cs = (torch.matmul(torch.t(source), source) - torch.matmul(torch.t(tmp_s), tmp_s) / (ns + eps)) / (ns - 1 + eps) * (
-                ns / (m + eps))
+    def MDA(self, source, target, bandwidths=[1]):
+        kernel_loss = self._mix_rbf_mmd2(source, target, sigmas=bandwidths) * 100.
+        eps = 1e-5
+        d = source.size()[1]
+        ns, nt = source.size()[0], target.size()[0]
 
-    # target covariance
-    tmp_t = torch.matmul(torch.ones(size=(1, nt)).cuda(), target)
-    ct = (torch.matmul(torch.t(target), target) - torch.matmul(torch.t(tmp_t), tmp_t) / (nt + eps)) / (nt - 1 + eps) * (
-                nt / (n + eps))
-    # frobenius norm
-    loss = torch.sqrt(torch.sum(torch.pow((cs - ct), 2)))
-    loss = loss / (4 * d * d) * 10.
+        # source covariance
+        tmp_s = torch.matmul(torch.ones(size=(1, ns)).cuda(), source)
+        cs = (torch.matmul(torch.t(source), source) - torch.matmul(torch.t(tmp_s), tmp_s) / (ns + eps)) / (
+                    ns - 1 + eps) * (
+                     ns / (self.m + eps))
 
-    return loss + kernel_loss
+        # target covariance
+        tmp_t = torch.matmul(torch.ones(size=(1, nt)).cuda(), target)
+        ct = (torch.matmul(torch.t(target), target) - torch.matmul(torch.t(tmp_t), tmp_t) / (nt + eps)) / (
+                    nt - 1 + eps) * (
+                     nt / (self.n + eps))
+        # frobenius norm
+        loss = torch.sqrt(torch.sum(torch.pow((cs - ct), 2)))
+        loss = loss / (4 * d * d) * 10.
 
-def _if_list(list):
-    if len(list) == 0:
-        list = torch.tensor(list)
-    else:
-        list = torch.vstack(list)
-    return list
+        return loss + kernel_loss
 
+    def CDA(self, output1, source_label, output2, pseudo_label):
+        s_0, s_1, s_2, s_3 = self._classification_division(output1, source_label)
+        t_0, t_1, t_2, t_3 = self._classification_division(output2, pseudo_label)
 
-def _classification_division(data, label):
-    N = data.size()[0]
-    a, b, c, d = [], [], [], []
-    for i in range(N):
-        if label[i] == 0:
-            a.append(data[i])   #.detach().cpu().numpy()
-        elif label[i] == 1:
-            b.append(data[i])
-        elif label[i] == 2:
-            c.append(data[i])
-        elif label[i] == 3:
-            d.append(data[i])
-
-    return _if_list(a), _if_list(b), _if_list(c), _if_list(d)
-
-
-def CDA(m, n, output1, source_label, output2, pseudo_label):
-    s_0, s_1, s_2, s_3 = _classification_division(output1, source_label)
-    t_0, t_1, t_2, t_3 = _classification_division(output2, pseudo_label)
-
-    CDA_loss = 0
-    if t_0.size()[0] != 0:
-        CDA_loss += MDA(m, n, s_0, t_0)
-    if t_1.size()[0] != 0:
-        CDA_loss += MDA(m, n, s_1, t_1)
-    if t_2.size()[0] != 0:
-        CDA_loss += MDA(m, n, s_2, t_2)
-    if t_3.size()[0] != 0:
-        CDA_loss += MDA(m, n, s_3, t_3)
-    return CDA_loss / 4.
+        CDA_loss = 0
+        if t_0.size()[0] != 0:
+            CDA_loss += self.MDA(s_0, t_0)
+        if t_1.size()[0] != 0:
+            CDA_loss += self.MDA(s_1, t_1)
+        if t_2.size()[0] != 0:
+            CDA_loss += self.MDA(s_2, t_2)
+        if t_3.size()[0] != 0:
+            CDA_loss += self.MDA(s_3, t_3)
+        return CDA_loss / 4.
 
 #############################################################33
 class I_Softmax(nn.Module):
@@ -214,7 +214,6 @@ class I_Softmax(nn.Module):
         label = torch.vstack(self.label_set)
         return data.cuda(), label.squeeze().cuda()
 
-
     def _class_angle(self, a, la):
 
         if len(la) == 0:
@@ -243,8 +242,6 @@ class I_Softmax(nn.Module):
         return data, label, loss
 
 
-
-
 ###################################################################################################3
 
 class DDTLN(nn.Module):
@@ -271,25 +268,27 @@ class DDTLN(nn.Module):
                                   nn.ReLU())
         self.p5_2 = nn.MaxPool1d(2, 2)
         self.p6 = nn.AdaptiveAvgPool1d(1)
-        self.p7_1 = nn.Sequential(nn.Linear(512, 128))
-        self.p7_2 = nn.Sequential(nn.utils.weight_norm(nn.Linear(128, 4), name='weight'))
+        self.p7_1 = nn.Sequential(nn.Linear(512, 512),
+                                  nn.ReLU())
+        self.p7_2 = nn.Sequential(nn.Linear(512, 4))
         # self._weights_init()
+
     def forward(self, x, y):
         x = self.p1_2(self.p1_1(x))
         x = self.p2_2(self.p2_1(x))
         x = self.p3_2(self.p3_1(x))
         x = self.p4_2(self.p4_1(x))
         x = self.p5_2(self.p5_1(x))
-        x1 = self.p7_1(self.p6(x).squeeze())
-        x2 = self.p7_2(x1)
+        x = self.p7_1(self.p6(x).squeeze())
+        x = self.p7_2(x)
         y = self.p1_2(self.p1_1(y))
         y = self.p2_2(self.p2_1(y))
         y = self.p3_2(self.p3_1(y))
         y = self.p4_2(self.p4_1(y))
         y = self.p5_2(self.p5_1(y))
-        y1 = self.p7_1(self.p6(y).squeeze())
-        y2 = self.p7_2(y1)
-        return x1, x2, y1, y2
+        y = self.p7_1(self.p6(y).squeeze())
+        y = self.p7_2(y)
+        return x, y
 
     def _weights_init(self):
         for L in self.modules():
@@ -304,13 +303,15 @@ class DDTLN(nn.Module):
                 if L.bias is not None:
                     L.bias.data.fill_(1)
 
-losses = []
-def train(model, epoch, source_loader, target_loader, optimizer):
 
+losses = []
+
+
+def train(model, epoch, source_loader, target_loader, optimizer):
     iter_source = iter(source_loader)
     iter_target = iter(target_loader)
     num_iter = len(source_loader)
-    lambd = -4 / (np.sqrt(epoch / (args.nepoch - epoch + 1)) + 1) + 4
+    lambd = 2 / (1 + np.exp(-10 * (epoch) / args.nepoch)) - 1
     for i in range(0, num_iter):
         source_data, source_label = next(iter_source)
         target_data, target_label = next(iter_target)
@@ -319,14 +320,13 @@ def train(model, epoch, source_loader, target_loader, optimizer):
         m = source_data.size()[0]
         n = target_data.size()[0]
         optimizer.zero_grad()
-        out1, output1, out2, output2 = model(source_data, target_data)                    #, output2, target_data
-        data, label, clc_loss_step = I_Softmax(3, 16, output1, source_label).forward()
+        output1, output2 = model(source_data.float(), target_data.float())  # , output2, target_data
+        data, label, clc_loss_step = I_Softmax(3, 0, output1, source_label).forward()
         pre_pseudo_label = torch.argmax(F.softmax(output2, dim=-1), dim=-1)
-        pseudo_data, pseudo_label, pseudo_loss_step = I_Softmax(3, 16, output2, pre_pseudo_label).forward()
-        # = criterion(output1, source_label)
-        CDA_loss = CDA(m, n, out1, source_label, out2, pre_pseudo_label)
-        MDA_loss = MDA(m, n, out1, out2)
-        loss_step = clc_loss_step + (MDA_loss + 0.5 * CDA_loss) + 10 * pseudo_loss_step
+        pseudo_data, pseudo_label, pseudo_loss_step = I_Softmax(3, 0, output2, pre_pseudo_label).forward()
+        CDA_loss = MMD(m, n).CDA(output1, source_label, output2, pre_pseudo_label)
+        MDA_loss = MMD(m, n).MDA(output1, output2)
+        loss_step = clc_loss_step + (MDA_loss + 0.1 * CDA_loss) + 0.1 * pseudo_loss_step
         loss_step.backward()
         optimizer.step()
         metric_accuracy_1.update(output1.max(1)[1], source_label)
@@ -342,7 +342,7 @@ def train(model, epoch, source_loader, target_loader, optimizer):
     ###############################################
     test_acc = metric_accuracy_2.compute()
     #################################################
-    train_all_loss = metric_mean_1.compute()    # loss_step
+    train_all_loss = metric_mean_1.compute()  # loss_step
     train_loss = metric_mean_2.compute()
     test_loss = metric_mean_3.compute()
     target_cla_loss = metric_mean_4.compute()
@@ -385,11 +385,11 @@ if __name__ == '__main__':
     target_loader = da.DataLoader(dataset=Train_target, batch_size=args.batch_size, shuffle=True, generator=g)
     model = DDTLN().to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     model.train()
     for epoch in range(0, args.nepoch):
         stop += 1
-        train_acc, test_acc, train_all_loss, train_loss, test_loss, target_cla_loss, source_cla_loss, cda_loss, mda_loss= train(
+        train_acc, test_acc, train_all_loss, train_loss, test_loss, target_cla_loss, source_cla_loss, cda_loss, mda_loss = train(
             model, epoch, source_loader, target_loader, optimizer)
         # if epoch == 1:
         #     for name, parms in model.named_parameters():
@@ -402,5 +402,7 @@ if __name__ == '__main__':
         #     test_acc = t_test_acc
         #     stop = 0
         #     torch.save(model, 'model.pkl')
-        print('Epoch{}, train_loss is {:.5f},test_loss is {:.5f}, train_accuracy is {:.5f},test_accuracy is {:.5f},train_all_loss is {:.5f},target_cla_loss is {:.5f},source_cla_loss is {:.5f},cda_loss is {:.5f},mda_loss is {:.5f}'.format(
-                    epoch + 1,train_loss,test_loss,train_acc,test_acc,train_all_loss,target_cla_loss,source_cla_loss,cda_loss,mda_loss))
+        print(
+            'Epoch{}, train_loss is {:.5f},test_loss is {:.5f}, train_accuracy is {:.5f},test_accuracy is {:.5f},train_all_loss is {:.5f},target_cla_loss is {:.5f},source_cla_loss is {:.5f},cda_loss is {:.5f},mda_loss is {:.5f}'.format(
+                epoch + 1, train_loss, test_loss, train_acc, test_acc, train_all_loss, target_cla_loss, source_cla_loss,
+                cda_loss, mda_loss))
